093cb1a81dfa66a0111a8c6d5199e1240790408f
suchitatripathi90-oss <suchitatripathi90@gmail.com>
2025-11-27 08:10:01 +0000

    working_datatype

--- .devcontainer/01_basic/02_datatypes.js
+++ .devcontainer/01_basic/02_datatypes.js
@@ -1,1 +1,11 @@
+ "use strick"; // treat all js code as newer version code//
+ 
+// alert(3+6) //  we r using nodejs, not browser 
+ 
+console.log(3+3); // not necessary to use ; //
+
+console.log(8
+    +5 // readablity is very important //
+);
+
 
