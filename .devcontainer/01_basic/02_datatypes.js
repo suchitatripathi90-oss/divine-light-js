836ac18fc3f6e4252869da7160a8e8f6bde30e73
suchitatripathi90-oss <suchitatripathi90@gmail.com>
2025-11-27 09:55:16 +0000

    datatype_done

--- .devcontainer/01_basic/02_datatypes.js
+++ .devcontainer/01_basic/02_datatypes.js
@@ -7,5 +7,26 @@ console.log(3+3); // not necessary to use ; //
 console.log(8
     +5 // readablity is very important //
 );
+let age =18 //integer
+let name = "wolf magic begin now" // string "" ,""" """, "" ""
+let isloggedIn = false // login or not
+let location ; // undefine
+
+// number = 2 to power 53
+//bigint = big no...2222222222.88888
+//boolean = true /false
+//string = ""
+//null =standlone value = empty
+// undefine = value not define
+// symbol = unique
+
+// object 
+ 
+console.log(typeof age); // typeof will tell data type of age or whatever u will ask
+console.log(typeof null); // null is object and output will be object
+console.log(typeof undefined);
+
+
+
 
 
