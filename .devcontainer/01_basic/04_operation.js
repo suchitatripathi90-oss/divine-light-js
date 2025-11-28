cd3879960cc1ca1f79d6c9097afeb3c6893049cf
suchitatripathi90-oss <suchitatripathi90@gmail.com>
2025-11-28 08:07:59 +0000

    done_operation.js

--- .devcontainer/01_basic/04_operation.js
+++ .devcontainer/01_basic/04_operation.js
@@ -1,1 +1,33 @@
+let value = 3
+let negValue = -value ;  // conert value in -
+console.log(negValue);
+
+console.log(518491617+518491617);
+console.log(518491617*518491617); // + _ * % /(reminder) **
+
+let string1 = "divine "
+let string2 = "light "
+let count = string1+ string2 // add string
+console.log(count);
+
+console.log("3" + 5 + 4)// out will come in str formate 354
+console.log(5 + 4 + "3");// 1st mathmatical oprtaion then add str =93
+console.log(true); // +true ,+"" = 0 
+
+let NumberCount = 9
+  NumberCount++ ; // This returns the value before incrementing.
+console.log(NumberCount);
+
+let b = 10
+console.log(++b); // outputs 71; the new value of b is produced
+console.log(b);// outputs 71; same as in case a
+
+  
+
+
+
+
+
+
+
 
