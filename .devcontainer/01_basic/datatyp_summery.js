e938b5233216d96b733af209019511c6d591abdd
suchitatripathi90-oss <suchitatripathi90@gmail.com>
2025-12-01 08:06:41 +0000

    working _datatype_summery

---
+++ .devcontainer/01_basic/datatyp_summery.js
@@ -0,0 +1,19 @@
+// primitive  7 types
+/*    these data call by value (through copy value) 
+String ,Number , Boolean , null , undefine 
+symbol (unique repration ) , bigInt (for big value- 518491617)
+*/
+ 
+const Id = Symbol('518491617')
+const NewId = Symbol('518491617')
+console.log(Id == NewId)
+
+
+//  Reference  ( non primitive  )
+
+/*  allocate memory in  reference 
+Array , Objects , Function    
+
+*/
+  
+console.log();
