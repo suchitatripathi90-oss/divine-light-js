85aa30a765bc5058e645aadd39410e6968f837ed
suchitatripathi90-oss <suchitatripathi90@gmail.com>
2025-12-01 08:22:41 +0000

    working

--- .devcontainer/01_basic/datatyp_summery.js
+++ .devcontainer/01_basic/datatyp_summery.js
@@ -7,13 +7,25 @@ symbol (unique repration ) , bigInt (for big value- 518491617)
 const Id = Symbol('518491617')
 const NewId = Symbol('518491617')
 console.log(Id == NewId)
+const bigNumber = (typeof 519491617519491617519491617n) 
+const bigNumber = 519491617519491617519491617n
+console.log(bigNumber);
 
 
 //  Reference  ( non primitive  )
 
 /*  allocate memory in  reference 
-Array , Objects , Function    
+Array [] , Objects {} , Function    
 
 */
-  
+ 
+ const  heros  = [ "mummy", "papa", " myself"] ;
+ let myobeject = {
+        name : "Suchita",
+        age : 36,
+        gender : false,
+}
+const nyFunction = function(){
+    console.log("divine light");
+}
 console.log();
