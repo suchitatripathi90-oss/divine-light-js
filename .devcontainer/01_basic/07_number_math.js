047ee3826324321394b02c11b7568789d284c34a
suchitatripathi90-oss <suchitatripathi90@gmail.com>
2025-12-02 10:22:49 +0000

    working_math

--- .devcontainer/01_basic/07_number_math.js
+++ .devcontainer/01_basic/07_number_math.js
@@ -1,2 +1,20 @@
-count score = 900;
+const score = 900;
 console.log(score );
+
+const count = new Number(518491617) // 9 number
+console.log(count);
+
+console.log(count.toString()); //converting no to string  and checking string fun.
+console.log(count.toString().length); // checking string lenght by fun.
+console.log(count.toFixed(2)); // after decimal we get 2 value 518491617.00
+
+ const newcount = 518491617.99
+console.log(newcount.toPrecision(9)); // 518491617.9 will precisise 518491618
+
+const hurdreds = 100000
+console.log(hurdreds.toLocaleString('en-In'));
+
+///// ****MATHS ****/////
+
+
+
