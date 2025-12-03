5bdf42ee58ceaa927750a93f639c9d86f00a8d68
suchitatripathi90-oss <suchitatripathi90@gmail.com>
2025-12-03 06:17:32 +0000

    done_num_math

--- .devcontainer/01_basic/07_number_math.js
+++ .devcontainer/01_basic/07_number_math.js
@@ -16,5 +16,22 @@ console.log(hurdreds.toLocaleString('en-In'));
 
 ///// ****MATHS ****/////
 
+console.log(Math);  // we check all math laibery Math.   then we can see all fun.
+console.log(Math.abs(-4));
+console.log(Math.round(8.95));
+console.log(Math.ceil(8.95));
+console.log(Math.floor(8.95))
+console.log(Math.max(8.95 ,77, 491))
+
+console.log(Math.random()); //math.random always gives value b/w 0-1
+console.log((Math.random()* 10)+1);
+console.log(Math.floor(Math.random()*10)+1);// floor means min & +1 means add 1 in that value
+const min = 10
+const max = 20 // for range we can give min and max value
+console.log(Math.floor(Math.random()* (max - min + 1))+ min); 
+
+
+
+
 
 
